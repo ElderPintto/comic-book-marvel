61c13e676d042972f59e7737bd36aae981d348db
elderpinto <eldersilvap@gmail>
2019-01-12 13:09:02 -0200

    validateUser retactoring and feacture logout

--- src/js/login.js
+++ src/js/login.js
@@ -1,77 +1,114 @@
 import $ from "jquery";
 // http://www.mocky.io/v2/5c39120f3100005d00a992bf
 
-class login {
-  constructor() {
 
-    this.users = [];
-    this.localSpeople = [];
-    this.binding();
-    this.init();
-  }
-  
-  getUsers() {
-    fetch("http://www.mocky.io/v2/5c39120f3100005d00a992bf")
-    .then(response => response.json()) // retorna uma promise
-    .then(result => {
-      this.users.push(result)
-    })
-    .catch(err => {
-    // trata se alguma das promises falhar
-    console.error('Failed retrieving information', err);
-  });
-  }
 
-  validadeUser(username, password) {
-    var _self = this;
-    this.users[0].forEach(function(element) {
-      console.log(element.username)
-      if (username == element.username && password == element.password) {
-        console.log(element.username + " is logged in!!!");
+  class login {
+    constructor() {
+      this.localSpeople = [];
+      this.init();
+      //this.autoLogin();
+    }
+    
+    getUsers(username,password) {
+      fetch("http://www.mocky.io/v2/5c39120f3100005d00a992bf")
+      .then(response => response.json()) // retorna uma promise
+      .then(result => {
+        console.log('getUsers',result)
 
-        _self.localSpeople.push({
-          username: element.username,
-          password: element.password
-        });
+        this.validadeUser(result, username, password)
+      })
+      .catch(err => {
+      // trata se alguma das promises falhar
+      console.error('Failed retrieving information', err);
+    });
+    }
 
-        _self.SetUserStorege(_self.localSpeople);
-        _self.redirectCatalog();
-      } else {
-        console.log("incorrect username or password");
+    validadeUser(users, username, password) {
+      var _self = this;
+
+      users.forEach(function(element) {
+
+        if (username == element.username && password == element.password) {
+          console.log(element.username + " is logged in!!!");
+
+          _self.localSpeople.push({
+            username: element.username,
+            password: element.password
+          });
+
+          _self.SetUserStorege(_self.localSpeople);
+          _self.redirectCatalog();
+        } else {
+          console.log("incorrect username or password");
+        }
+      });
+    }
+
+    SetUserStorege(user) {
+      let userStorage = JSON.stringify(user);
+      localStorage.setItem("loggedUser", userStorage);
+      localStorage.setItem("logged", "true");
+    }
+
+    redirectCatalog(){
+      window.location.replace("http://localhost:9000/catalog.html");
+    }
+
+    autoLogin() {
+      let _self = this;
+      let logged = localStorage.getItem("logged");
+      
+      let user = localStorage.getItem("loggedUser");
+      let loggedUser = JSON.parse(user);
+
+      if(logged == "true") {
+        
+        if(user){
+          this.getUsers(loggedUser[0].username, loggedUser[0].password)
+        }
       }
-    });
+    
+
+    }
+
+    logout() {
+      localStorage.setItem("logged", "false");
+      localStorage.removeItem("loggedUser");
+      window.location.replace("http://localhost:9000");
+    }
+
+    bindEvents() {
+      let _self = this;
+      let $username = $("#username");
+      let $password = $("#password");
+      let $btnSend = $("#send");
+
+      $(".form").submit("#send", function(e) {
+        e.preventDefault();
+        let valName = $username.val();
+        let valPass = $password.val();
+
+        console.log(valName)
+        console.log(valPass)
+
+        _self.getUsers(valName, valPass);
+      });
+
+      $('.form').on('click', '#logout', _self.logout)
+
+      $(document).ready(function() {
+
+        if(localStorage.getItem("logged") != 'true'){
+          localStorage.setItem("logged", "false");
+        }
+
+      });
+    }
+
+    init() {
+      this.bindEvents();
+    }
   }
 
-  SetUserStorege(user) {
-    let userStorage = JSON.stringify(user);
-    localStorage.setItem("logged", userStorage);
-  }
-
-  redirectCatalog(){
-    window.location.replace("http://localhost:9000/catalog.html");
-  }
-
-  binding() {
-    let _self = this;
-    let $username = $("#username");
-    let $password = $("#password");
-    let $btnSend = $("#send");
-
-    $(".form").submit("#send", function(e) {
-      e.preventDefault();
-      let valName = $username.val();
-      let valPass = $password.val();
-
-      console.log(valName)
-      console.log(valPass)
-
-      _self.validadeUser(valName, valPass);
-    });
-  }
-
-  init() {
-    this.getUsers()
-  }
-}
-
-new login();
+  new login();
